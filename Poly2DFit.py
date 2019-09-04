434bf52aeae320e508047aa39605718ef6eee8a9
Lukas Materne <lukas_materne@yahoo.de>
2019-09-04 12:37:34 +0200

    update of Poly2DFit and example of usage in project1_main.py

--- Poly2DFit.py
+++ Poly2DFit.py
@@ -1,5 +1,6 @@
 from additional_functions_project1 import MSE, R2, FrankeFunction
 import numpy as np 
+import subprocess
 
   
 class Poly2DFit:
@@ -18,7 +19,6 @@ class Poly2DFit:
         -_linReg and _ridgeReg calculate parameters with respectivly regression type and calculate parameter variance
         - runFit performes the fit
     """
-
     def generateSample(self, n, mean = 0, var = 1):
         """
         Generates (n,3) samples [x,y,z], where x,y are uniform random numbers [0,1) 
@@ -37,77 +37,77 @@ class Poly2DFit:
         self.data = f
 
     def matDesign (self, indVariables = 2):
-    '''This is a function to set up the design matrix
-    the inputs are :dataSet, the n datapoints, x and y data in a nx2 matrix
-                    order, is the order of the coefficients, 
-                    indVariables, the number of independant variables
+        '''This is a function to set up the design matrix
+        the inputs are :dataSet, the n datapoints, x and y data in a nx2 matrix
+                        order, is the order of the coefficients, 
+                        indVariables, the number of independant variables
+                        
+        i.e if order = 3 and indVariables = 1, then the number of coefficients THIS function will create is 4. (1 x x**2 x**3)
+        or  if order = 2 and indVariables = 2, then the number of coefficients THIS function will create is 6. (1 x y xy x**2 y**2) 
+        
+        IMPORTANT NOTE: this works only for indVariables = 2 at the moment
+        
+        the outputs are X
+        '''
+        #stack data
+        dataSet = np.vstack((self.x, self.y)).T
+
+        # if statement for the case with one independant variable
+        if indVariables == 1:
+            num_coeff = int(self.order + 1)
+            
+            # set up the Design matrix
+            #n = np.int(np.size(dataSet))
+            #matX = np.zeros((n,coefficients))
+            
+            n = np.shape(dataSet)[0]
+            # loop through all the other columns as powes of dataSet
+            
+            self._design = np.zeros((n,num_coeff))
+            i = 0 #counter
+            while i < num_coeff:
+
+                self._design[:,i] = (dataSet[i])**i
+                i=i+1
+            
+            
+        ###########################################################################################################
+        
+        # if statement for the case with two independant variables
+        
+        if (indVariables == 2):
+            # find the number of coefficients we will end up with
+            num_coeff = int((self.order + 1)*(self.order + 2)/2)
+            #print ('The number of coefficients are: ',num_coeff)
                     
-    i.e if order = 3 and indVariables = 1, then the number of coefficients THIS function will create is 4. (1 x x**2 x**3)
-    or  if order = 2 and indVariables = 2, then the number of coefficients THIS function will create is 6. (1 x y xy x**2 y**2) 
-    
-    IMPORTANT NOTE: this works only for indVariables = 2 at the moment
-    
-    the outputs are X
-    '''
-    #stack data
-    dataSet = np.vstack(self.x, self.y).T
+            #find the number of rows in dataSet
+            n = np.shape(dataSet)[0]
+            #print ('The number of rows in the design matrix is', n)
+            # create an empty matrix of zeros
+            self._design = np.zeros((n,num_coeff))
+            
 
-    # if statement for the case with one independant variable
-    if indVariables == 1:
-        num_coeff = int(self.order + 1)
-        
-        # set up the Design matrix
-        #n = np.int(np.size(dataSet))
-        #matX = np.zeros((n,coefficients))
-        
-        n = np.shape(dataSet)[0]
-        # loop through all the other columns as powes of dataSet
-        
-        self._design = np.zeros((n,num_coeff))
-        i = 0 #counter
-        while i < num_coeff:
-
-            self.design[:,i] = (dataSet[i])**i
-            i=i+1
-        
-        
-    ###########################################################################################################
-    
-    # if statement for the case with two independant variables
-    
-    if (indVariables == 2):
-        # find the number of coefficients we will end up with
-        num_coeff = int((self.order + 1)*(self.order + 2)/2)
-        #print ('The number of coefficients are: ',num_coeff)
-                
-        #find the number of rows in dataSet
-        n = np.shape(dataSet)[0]
-        #print ('The number of rows in the design matrix is', n)
-        # create an empty matrix of zeros
-        self._design = np.zeros((n,num_coeff))
-        
-
-        
-        col_G = 0 # global columns        
-        tot_rows = n
-        #print ('total rows = ',tot_rows)
-        
-        j = 0        
-        # loop through each j e.g 1,2,3,4,5,6
-        while j < num_coeff:
-            k = 0
-            #loop through each row
-            while k <= j:                   
-                row = 0                
-                #loop through each item (each column in the row)
-                while row < tot_rows:
-                    self._design[row,col_G] = ((dataSet[row,0])**(j-k)) * ((dataSet[row,1])**k)                                        
-                    row = row + 1 
-                    #print(row)
-                    
-                k = k + 1                     
-            col_G = col_G + 1            
-            j = j + 1 
+            
+            col_G = 0 # global columns        
+            tot_rows = n
+            #print ('total rows = ',tot_rows)
+            
+            j = 0        
+            # loop through each j e.g 1,2,3,4,5,6
+            while j < num_coeff:
+                k = 0
+                #loop through each row
+                while k <= j:                   
+                    row = 0                
+                    #loop through each item (each column in the row)
+                    while row < tot_rows:
+                        self._design[row,col_G] = ((dataSet[row,0])**(j-k)) * ((dataSet[row,1])**k)                                        
+                        row = row + 1 
+                        #print(row)
+                        
+                    k = k + 1                     
+                col_G = col_G + 1            
+                j = j + 1 
 
     def _linReg(self):
         """
@@ -125,7 +125,7 @@ class Poly2DFit:
         outputs variance as the diagonal entries of (X^TX- lam I)^-1
         """
         #creating identity matrix weighted with lam
-        diag = self.lam * np.ones(design.shape[1])
+        diag = self.lam * np.ones(self._design.shape[1])
         inverse = np.linalg.inv(self._design.T.dot(self._design) + np.diag(diag))
         self.par_var = np.diag(inverse)
         self.par = inverse.dot(self._design.T).dot(self.data)
@@ -140,12 +140,13 @@ class Poly2DFit:
         """
         self.order = Pol_order
         self.lam = lam
+        self.regType = regtype
         Poly2DFit.matDesign(self)
 
         if regtype == 'OLS':
-            _linReg(data, design)
+            Poly2DFit._linReg(self)
         if regtype == 'RIDGE':
-            _ridgeReg(data, design, lam)
+            Poly2DFit._ridgeReg(self)
 
         return self.par, self.par_var
 
@@ -161,8 +162,27 @@ class Poly2DFit:
         expect_model = np.mean(model)
 
         self.mse = MSE(self.data, model)
-        self.r2 = R2(data, model)
+        self.r2 = R2(self.data, model)
 
         self.bias = MSE( self.data, expect_model)
         self.variance = MSE(model, expect_model)
         return self.x, self.y, model
+
+    def store_information(self, filepath, filename):
+    
+        try:
+            f = open(filepath + "/"+ regtype + ad_lam + ".txt",'w+')
+        except:
+            subprocess.call(["mkdir", "-p", filepath ])
+            f = open(filepath + "/"+ filename + ".txt",'w+')
+
+        f.write("    Perfomance of %s regression with  %i parameters \n:" %(self.regType, len(self.par)))
+        
+        if self.regType != 'OLS':
+            f.write("Regularization parameter lambda = %f\n" %self.lam)
+        
+        f.write("MSE = %.4f \t R2 = %.4f \t Bias(model)=%.4f \t Variance(model) =%.4f \n" %(self.mse, self.r2, self.bias, self.variance))
+        f.write("Parameter Information:\n")
+        for i in range(len(self.par)):
+            f.write("beta_%i = %.4f +- %.4f\n" %(i, self.par[i], np.sqrt(self.par_var[i])) )
+        f.close()
--- project1_main.py
+++ project1_main.py
@@ -1,79 +1,28 @@
-from additional_functions_project1 import generate_sample, variance_bias_tradeoff
-from functions_project1 import run_fit, matDesign, run_fit_split
-
+import Poly2DFit
 import numpy as np
 
 def main():
-    n = 1000
-    maxorder = 5
+    n = 10000
+    order =3
 
-    x,y,z = generate_sample(n)
+    #creat fit object
+    fit_object = Poly2DFit.Poly2DFit()
+    #generate data with noise: mean 0, var =1
+    fit_object.generateSample(n)
+    #run polynomial fit of order 3, OLS type
+    #returns the fitted parameters and theire variance
+    par, par_var = fit_object.run_fit( order, 'OLS'  )
+    #evaluate model, return x,y points and model prediction
+    x, y, fit = fit_object.evaluate_model()
+    #stores information about the fit in ./Test/OLS/first_OLS.txt
+    fit_object.store_information('Test/OLS','first_OLS')
 
-    p = np.arange(0,maxorder+1)
-    fit = ['OLS', 'RIDGE']
-    lam = [0.01, 0.1, 0.5, 1]
-
-    mod_var_ridge, mod_bias_ridge, mse_ridge = np.zeros((3, maxorder+1, len(lam)))
-    mse_ols, mod_var_ols, mod_bias_ols = np.zeros((3, maxorder +1))
+    #same procedure but now as ridge wit lambda = 0.1
+    par, par_var = fit_object.run_fit( order, 'RIDGE', 0.1  )
+    #evaluate model, return x,y points and model prediction
+    x, y, fit = fit_object.evaluate_model()
+    #stores information about the fit in ./Test/OLS/first_OLS.txt
+    fit_object.store_information('Test/RIDGE','first_RIDGE')
     
-    #part a)######################################################################
-    #calc models
-    for i,p in enumerate(p):
-        design = matDesign([x,y], p, 2)
-        for f in fit:
-            if f != 'OLS':
-                for j,l in enumerate(lam):
-                    ret = run_fit(z, design, f, l, filepath='part_a_pol%i'%i)
-                    mod_var_ridge[i,j] = ret['Model_Variance']
-                    mod_bias_ridge[i,j] = ret['Model_Bias']
-                    mse_ridge[i,j] = ret['MSE']
-            else:
-                ret = run_fit(z, design, f, filepath='part_a_pol%i'%i)
-                mod_var_ols[i] = ret['Model_Variance']
-                mod_bias_ols[i] = ret['Model_Bias']
-                mse_ols[i] = ret['MSE']
-    #plot models
-    for f in fit:
-        if f != 'OLS':
-            for i, l in enumerate(lam):
-                variance_bias_tradeoff(p, mse_ridge[:,i], mod_var_ridge[:,i],
-                                         mod_bias_ridge[:,i], f + str(l),
-                                         filepath='results_part_a',title='Lambda=%.2f'%l)
-
-        else:
-            variance_bias_tradeoff(p, mse_ols, mod_var_ols, mod_bias_ols, f + str(l),
-                                    filepath='results_part_a')
-
-    #part b)#############################################################################
-    for i,p in enumerate(p):
-        design = matDesign([x,y], p, 2)
-        for f in fit:
-            if f != 'OLS':
-                for j,l in enumerate(lam):
-                    ret = run_fit_split(z, design, f, l, filepath='part_b_pol%i'%i)
-                    
-                    ###put eval model
-                    mod_var_ridge[i,j] = ret['Model_Variance']
-                    mod_bias_ridge[i,j] = ret['Model_Bias']
-                    mse_ridge[i,j] = ret['MSE']
-            else:
-                ret = run_fit_split(z, design, f, filepath='part_b_pol%i'%i)
-                # put eval model
-                mod_var_ols[i] = ret['Model_Variance']
-                mod_bias_ols[i] = ret['Model_Bias']
-                mse_ols[i] = ret['MSE']
-    #plot models
-    for f in fit:
-        if f != 'OLS':
-            for i, l in enumerate(lam):
-                variance_bias_tradeoff(p, mse_ridge[:,i], mod_var_ridge[:,i],
-                                         mod_bias_ridge[:,i], f + str(l),
-                                         filepath='results_part_b',title='Lambda=%.2f'%l)
-
-        else:
-            variance_bias_tradeoff(p, mse_ols, mod_var_ols, mod_bias_ols, f + str(l),
-                                    filepath='results_part_b')
-    
-
 if __name__ == "__main__":
     main()
