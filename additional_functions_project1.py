23b7ac529fdad9a7884918bd896789606e6b5356
jamesbc123 <54575038+jamesbc123@users.noreply.github.com>
2019-09-06 09:51:24 +0200

    Added function for plot, function for split,
    Also I changed model to self.model so that we can use that in main.py
    
    I commented out the store information as I recieved some errors
    
    The function for plot doesn't work however as it says Z needs to be of 2 dimensions, this is really strange

--- additional_functions_project1.py
+++ additional_functions_project1.py
@@ -1,5 +1,11 @@
 #additional functions
 import numpy as np
+from sklearn.utils import resample
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from sklearn.model_selection import train_test_split
 
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
@@ -25,5 +31,44 @@ def R2(data, model):
     numerator = MSE(data, model)
     denominator = MSE(data, np.mean(data))
     return 1.0 - numerator/denominator
+
+def plot_it(x,y,z):
+    '''
+    This is a function to plot the x y and z data
+    Inputs: z-axis data
+    
+    This function takes is the zdata only as x and y are assumed to stay the same
+    the zdata this could be from the franke function or the model,
+    you just need to specify
+    
+    Outputs: This function returns nothing    
+    
+    '''    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x,y,z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    
+    # Customize the z axis.
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+def split(x,y):
+    '''This is a function to resample the data into training data for the model and test data.
+    Inputs: x data from the full set
+            y data from the full set
+    Outputs: its outputs the xtrain, xtest, ytrain, ytest in this order
+            xtrain will have 0.66 of the full data set
+            xtest will have 0.33 of the full data set
+    
+    '''
+    
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, train_size=0.66, random_state=None)
+    
+    return xtrain, xtest, ytrain, ytest
 
     
--- project1_main.py
+++ project1_main.py
@@ -1,28 +1,40 @@
 import Poly2DFit
 import numpy as np
 
+
 def main():
-    n = 10000
+    n = 10
     order =3
 
     #creat fit object
     fit_object = Poly2DFit.Poly2DFit()
     #generate data with noise: mean 0, var =1
-    fit_object.generateSample(n)
+    fit_object.generate_split_Sample(n)
+    print('this is the split x and y', fit_object.x,fit_object.y)
+    #split the sample 
+    fit_object.split()
     #run polynomial fit of order 3, OLS type
     #returns the fitted parameters and theire variance
     par, par_var = fit_object.run_fit( order, 'OLS'  )
     #evaluate model, return x,y points and model prediction
     x, y, fit = fit_object.evaluate_model()
+    
+    print(fit_object.x)  
     #stores information about the fit in ./Test/OLS/first_OLS.txt
-    fit_object.store_information('Test/OLS','first_OLS')
+#    fit_object.store_information('Test/OLS','first_OLS')
 
     #same procedure but now as ridge wit lambda = 0.1
     par, par_var = fit_object.run_fit( order, 'RIDGE', 0.1  )
     #evaluate model, return x,y points and model prediction
     x, y, fit = fit_object.evaluate_model()
     #stores information about the fit in ./Test/OLS/first_OLS.txt
-    fit_object.store_information('Test/RIDGE','first_RIDGE')
+#    fit_object.store_information('Test/RIDGE','first_RIDGE')
+    
+    print('zdata from franke is shape', np.shape(fit_object.data))
+    fit_object.plot_function(fit_object.data)
+    
+    
+    
     
 if __name__ == "__main__":
     main()
