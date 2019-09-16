fb4a9039d799dc119543fa850e5c0d27272ebeee
Lasse Steinnes <lasse.steinnes93@gmail.com>
2019-09-16 11:45:57 +0200

    added load_terrain

--- additional_functions_project1.py
+++ additional_functions_project1.py
@@ -7,6 +7,12 @@ from mpl_toolkits import mplot3d
 
 from sklearn.model_selection import train_test_split
 
+from imageio import imread
+
+from mpl_toolkits.mplot3d import Axes3D
+
+from matplotlib import cm
+
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -41,21 +47,34 @@ def plot_it(x,y,model,franke_data):
     This is a function to plot the x y and z data
     Inputs: x: the generated x points
             y: the generated y points
-            model: the model we are testing 
+            model: the model we are testing
             franke_data : the data from the frankefunction
-    '''       
+    '''
     ax = plt.axes(projection='3d')
-    
+
     # plots scatter and trisurf
     ax.plot_trisurf(x, y, model, cmap='viridis', edgecolor='none')
     ax.scatter(x,y,franke_data)
-    
+
     #set the axis labels and title
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     ax.set_title('The sample data plotted as a scatter & the model plotted as a trisurf')
-  
+
     plt.show()
 
-    
+
+def load_terrain(imname):
+# Load the terrain
+    terrain = imread('{:}.tif'.format(imname))
+    terraindata = misc.imread('{:}.tif'.format(imname))
+# Show the terrain
+    plt.figure()
+    plt.title('Terrain over Norway')
+    plt.imshow(terrain, cmap='gray')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+# return the terrain for plotting
+    return terrain,terraindata
