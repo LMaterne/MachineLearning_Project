48ec5ca69c239202b6c6500becb27c642d39d532
Lasse Steinnes <lasse.steinnes93@gmail.com>
2019-09-16 09:58:59 +0200

    Added lasso
    Check if correct.

--- Poly2DFit.py
+++ Poly2DFit.py
@@ -1,10 +1,10 @@
 from additional_functions_project1 import MSE, R2, FrankeFunction, plot_it
-import numpy as np 
+import numpy as np
 import subprocess
 import warnings
 from sklearn.model_selection import train_test_split
 
-  
+
 class Poly2DFit:
     """
     class which perfoms a 2D polynomial fit to given data or generated samples from the Franke function
@@ -26,7 +26,7 @@ class Poly2DFit:
         This function creates a sample [x,y,z] where x,y are uniform random numbers [0,1)
         and z = f(x,y) + eps with f the Franke function and eps normal distributed with mean and var
         This function also has the option to create a kfold split into test and training sets
-        
+
         Inputs: n:          the number of data points
                 gen_type:   this is either split or nosplit i.e train test split yes or no
                 kfold:      the number of fold to splits the population into ,usually 5 or 10
@@ -35,46 +35,46 @@ class Poly2DFit:
                 y:          y points
                 data:       x and y passed into the franke function
         """
-        
+
         self.genType = gentype
         #use same random numbers each time to make evaulating easier
         np.random.seed(0)
-        #create x and y randomly 
+        #create x and y randomly
         x_temp, y_temp = np.random.rand(2,n)
-        
+
         if gentype == 'nosplit':
             self.x = x_temp
-            self.y = y_temp 
+            self.y = y_temp
             #pass the x and y data into the Franke function this will be used later in evaluating the model
             self.data = FrankeFunction(self.x, self.y) + np.sqrt(var)*np.random.randn(n) + mean
-            
+
         if gentype == 'split':
             # the train set size is e.g 4/5 if kfold is 5, hence test size is 1/kfold
             xtrain, xtest, ytrain, ytest = train_test_split(x_temp, y_temp, shuffle = True, test_size= (1/kfold))
-            
+
             # the x and y training data will be used to create the model
             self.x = xtrain
             self.y = ytrain
             self.xtest = xtest
             self.ytest = ytest
-            
+
             #find the length of the training and test data
             ntrain = np.shape(xtrain)[0]
             ntest = np.shape(xtest)[0]
-            
+
             #pass the training and test data into the Franke function this will be used later in evaluating the model
             self.data = FrankeFunction(self.x, self.y) + np.sqrt(var)*np.random.randn(ntrain) + mean
             self.datatest = FrankeFunction(self.xtest, self.ytest) + np.sqrt(var)*np.random.randn(ntest) + mean
-    
+
     def givenData(self, x, y, f):
         """
         stores given 2D data in class
         x,y are dependent variables, f= f(x,y)
-        """        
+        """
         self.x = x
         self.y = y
         self.data = f
- 
+
     def run_fit(self, Pol_order, regtype, lam = 0.1):
         """
         perfomes the fit of the data to the model given as design matrix
@@ -93,7 +93,7 @@ class Poly2DFit:
             Poly2DFit._ridgeReg(self)
 
         return self.par, self.par_var
- 
+
     def _linReg(self):
         """
         calculates the estimated parameters of an OLS
@@ -114,7 +114,7 @@ class Poly2DFit:
 
     def _ridgeReg(self):
         """
-        returns the estimated parameters of an Ridge Regression with 
+        returns the estimated parameters of an Ridge Regression with
         regularization parameter lambda
         outputs variance as the diagonal entries of (X^TX- lam I)^-1
         """
@@ -132,18 +132,39 @@ class Poly2DFit:
 
         self.par_var = np.diag(inverse)
         self.par = inverse.dot(self._design.T).dot(self.data)
-       
+
+
+    def _lasso(self):
+        """
+        Creates a model using Lasso, Returns the estimated output z
+        """
+          lasso = Lasso(alpha=self.lam, max_iter=10e5)
+          # creates the Lasso parameters
+          # Using train set X, f
+          self.clf_l = lasso.fit(self.design,self.data)
+          # Using test set f to predict f
+          self.f_tilde_lasso = clf_l.predict(self.design)
+"""
+ might use these to get an overview.
+    train_score=lasso.score(X_train,z_train)
+    test_score=lasso.score(X_test,z_test)
+    coeff_used = np.sum(lasso.coef_!=0)
+    print("training score:", train_score)
+    print("test score: ", test_score)
+    print("number of features used: ", coeff_used)
+""""
+
+
     def matDesign (self, x , y , indVariables = 2):
         '''This is a function to set up the design matrix
         the inputs are :dataSet, the n datapoints, x and y data in a nx2 matrix
-                        order, is the order of the coefficients, 
+                        order, is the order of the coefficients,
                         indVariables, the number of independant variables
-                        
+
         i.e if order = 3 and indVariables = 1, then the number of coefficients THIS function will create is 4. (1 x x**2 x**3)
-        or  if order = 2 and indVariables = 2, then the number of coefficients THIS function will create is 6. (1 x y xy x**2 y**2) 
-        
+        or  if order = 2 and indVariables = 2, then the number of coefficients THIS function will create is 6. (1 x y xy x**2 y**2)
+
         IMPORTANT NOTE: this works only for indVariables = 1, 2 at the moment
-        
         the outputs are X
         '''
         #stack data
@@ -152,60 +173,60 @@ class Poly2DFit:
         # if statement for the case with one independant variable
         if indVariables == 1:
             num_coeff = int(self.order + 1)
-            
+
             # set up the Design matrix
             #n = np.int(np.size(dataSet))
             #matX = np.zeros((n,coefficients))
-            
+
             n = np.shape(dataSet)[0]
             # loop through all the other columns as powes of dataSet
-            
+
             self._design = np.zeros((n,num_coeff))
             i = 0 #counter
             while i < num_coeff:
 
                 self._design[:,i] = (dataSet[i])**i
                 i=i+1
-            
-            
+
+
         ###########################################################################################################
-        
+
         # if statement for the case with two independant variables
-        
+
         if (indVariables == 2):
             # find the number of coefficients we will end up with
             num_coeff = int((self.order + 1)*(self.order + 2)/2)
             #print ('The number of coefficients are: ',num_coeff)
-                    
+
             #find the number of rows in dataSet
             n = np.shape(dataSet)[0]
             #print ('The number of rows in the design matrix is', n)
             # create an empty matrix of zeros
             self._design = np.zeros((n,num_coeff))
-            
 
-            
-            col_G = 0 # global columns        
+
+
+            col_G = 0 # global columns
             tot_rows = n
             #print ('total rows = ',tot_rows)
-            
-            j = 0        
+
+            j = 0
             # loop through each j e.g 1,2,3,4,5,6
             while j < num_coeff:
                 k = 0
                 #loop through each row
-                while k <= j:                   
-                    row = 0                
+                while k <= j:
+                    row = 0
                     #loop through each item (each column in the row)
                     while row < tot_rows:
-                        self._design[row,col_G] = ((dataSet[row,0])**(j-k)) * ((dataSet[row,1])**k)                                        
-                        row = row + 1 
+                        self._design[row,col_G] = ((dataSet[row,0])**(j-k)) * ((dataSet[row,1])**k)
+                        row = row + 1
                         #print(row)
-                        
-                    k = k + 1                     
-                col_G = col_G + 1            
-                j = j + 1 
- 
+
+                    k = k + 1
+                col_G = col_G + 1
+                j = j + 1
+
     def evaluate_model(self):
         """
         -calculates the MSE
@@ -213,50 +234,50 @@ class Poly2DFit:
         returns the modelpoints
         """
         p = self.par.shape[0]
-        
+
         if self.genType == 'split':
-            
+
             Poly2DFit.matDesign(self, self.xtest, self.ytest)
             self.model = self._design.dot(self.par)
-            
+
             expect_model = np.mean(self.model)
-    
+
             self.mse = MSE(self.datatest, self.model)
             self.r2 = R2(self.datatest, self.model)
-          
+
             #self.bias = MSE(FrankeFunction(self.xtest, self.ytest), expect_model) # explain this in text why we use FrankeFunction
             self.variance = MSE(self.model, expect_model)
             #alternative implementaton
             # MSE = bias + variance + data_var <-> bias = MSE - varinace - data_var
             #what is data var?
             self.bias = self.mse - self.variance - np.var([self.x, self.y])
-        
+
         if self.genType == 'nosplit':
             Poly2DFit.matDesign(self, self.x, self.y)
             self.model = self._design.dot(self.par)
-        
+
             expect_model = np.mean(self.model)
-        
+
             self.mse = MSE(self.data, self.model)
             self.r2 = R2(self.data, self.model)
-          
+
             self.bias = MSE(FrankeFunction(self.x, self.y), expect_model) # explain this in text why we use FrankeFunction
             self.variance = MSE(self.model, expect_model)
-       
+
         return self.x, self.y, self.model
-    
-   
+
+
     def plot_function(self):
         """
-        This functions: 
+        This functions:
         -plots the x,y and franke function data in a scatter plot
         -plots the x,y and model in a triangulation plot
         """
         self.plot_function = plot_it(self.x, self.y, self.model, self.data)
-         
+
 
     def store_information(self, filepath, filename):
-        
+
         try:
             f = open(filepath + "/" + filename  + ".txt",'w+')
         except:
@@ -264,10 +285,10 @@ class Poly2DFit:
             f = open(filepath + "/"+ filename + ".txt",'w+')
 
         f.write("    Perfomance of %s regression with  %i parameters \n:" %(self.regType, len(self.par)))
-        
+
         if self.regType != 'OLS':
             f.write("Regularization parameter lambda = %f\n" %self.lam)
-        
+
         f.write("MSE = %.4f \t R2 = %.4f \t Bias(model)=%.4f \t Variance(model) =%.4f \n" %(self.mse, self.r2, self.bias, self.variance))
         f.write("Parameter Information:\n")
         for i in range(len(self.par)):
